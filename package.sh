a407ca7273ba7655925745333aefc53e456042aa
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-05-28 22:56:35 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 cu=`pwd`
 rm GOARCH* && echo "success deletion" || echo "not_success"
-rm -rf $1/release/packages && echo "success deletion" || echo "not success"
-mkdir -p $1/release/packages/
+rm -rf release/packages && echo "success deletion" || echo "not success"
+mkdir -p release/packages/
 os_all='linux windows darwin freebsd'
 arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle'
 for os in $os_all; do
@@ -9,11 +9,11 @@ for os in $os_all; do
       set GOOS=$os
       set GOARCH=$arch
       if [ $os = "windows" ]; then
-        go build -o $1"_"$os"_"$arch".exe" && echo "Success build for arch "$arch" and os "$os || echo "No problem"
-        mv $1"_"$os"_"$arch".exe" $1/release/packages && echo "Move success" || echo "Move not success"
+        go build -o $os"_"$arch".exe" && echo "Success build for arch "$arch" and os "$os || echo "No problem"
+        mv $os"_"$arch".exe" release/packages && echo "Move success" || echo "Move not success"
       else
-        go build -o $1"_"$os"_"$arch && echo "Success build for arch "$arch" and os "$os || echo "No problem"
-        mv $1"_"$os"_"$arch".exe" $1/release/packages && echo "Move success" || echo "Move not success"
+        go build -o $os"_"$arch && echo "Success build for arch "$arch" and os "$os || echo "No problem"
+        mv $os"_"$arch".exe" release/packages && echo "Move success" || echo "Move not success"
       fi
     done
 done
