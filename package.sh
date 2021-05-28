bd0849c6a174fe292f3dec8c4f2582b49f6c5234
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-05-28 22:48:10 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,15 +3,16 @@ rm -rf $1/release/packages && echo "success deletion" || echo "not success"
 mkdir -p $1/release/packages/
 os_all='linux windows darwin freebsd'
 arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle'
-cd $1/release/packages
 for os in $os_all; do
     for arch in $arch_all; do
       set GOOS=$os
       set GOARCH=$arch
       if [ $os = "windows" ]; then
         go build -o $1"_"$os"_"$arch".exe" && echo "Success build for arch "$arch" and os "$os || echo "No problem"
+        mv $1"_"$os"_"$arch".exe" $1/release/packages && echo "Move success" || echo "Move not success"
       else
         go build -o $1"_"$os"_"$arch && echo "Success build for arch "$arch" and os "$os || echo "No problem"
+        mv $1"_"$os"_"$arch".exe" $1/release/packages && echo "Move success" || echo "Move not success"
       fi
     done
 done
